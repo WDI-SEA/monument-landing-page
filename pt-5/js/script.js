5fed74d60aa4682e5951232ed1f266d922fd058e
maria-christina-sevilla <m.christina.sevilla@gmail.com>
2017-04-10 20:25:53 -0700

    used jQuery val() method

--- pt-5/js/script.js
+++ pt-5/js/script.js
@@ -1,23 +1,17 @@
 $(function() {
-    console.log("jQuery works!");
+
 
     $(".submit").on('click', function(event) {
         event.preventDefault();
-        console.log($("submit clicked"));
-        // what do we do when this is clicked
-        console.log($(".edited-element"));
 
         $(".edited-element").each(function() {
-            // console.log(this);
-            // console.log($(this).val());
-            // console.log($(this).val());
+
             if ($(this).val()) {
                 // value is non-blank
                 $(this).removeClass("error");
                 $(this).siblings().hide();
             } else {
                 // value is blank
-                // console.log($(this).val() + " is blank");
                 $(this).addClass("error");
                 $(this).siblings().fadeIn(1000);
             }
