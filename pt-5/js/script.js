2257f5ccc355cd0d7fb58b383c98ba62b5e9e9d0
maria-christina-sevilla <m.christina.sevilla@gmail.com>
2017-04-10 19:59:38 -0700

    edited comment form

---
+++ pt-5/js/script.js
@@ -0,0 +1,44 @@
+$(function() {
+    console.log("jQuery works!");
+
+    $(".submit").on('click', function(event) {
+        event.preventDefault();
+        console.log($("submit clicked"));
+        // what do we do when this is clicked
+        console.log($(".edited-element"));
+
+        $(".edited-element").each(function() {
+            console.log(this);
+            console.log(this.value);
+            if (this.value) {
+                // value is non-blank
+                $(this).removeClass("error");
+                $(this).siblings().hide();
+            } else {
+                // value is blank
+                console.log(this.value + " is blank");
+                $(this).addClass("error");
+                $(this).siblings().fadeIn(1000);
+            }
+        });
+
+
+    });
+
+});
+
+
+// $("form").on("submit", function(event) {
+//         var queryText = $("query-input").val();
+//         console.log(queryText);
+
+//         if (queryText.length < 5) {
+//             event.preventDefault();
+
+//             $('#query-input-error-message').text(`Query sting must be at least 5 character long. "${queryText}" is only ${queryText.length} characters long`);
+
+//         } else {
+//             // go to google!
+//         }
+//     }
+// });
---
+++ pt-5/script.js
@@ -0,0 +1,23 @@
+$(function() {
+            console.log("jQuery works!");
+
+        }
+
+
+        //     $("form").on("submit", function(event) {
+        //             var queryText = $("query-input").val();
+        //             console.log(queryText);
+
+        //             if (queryText.length < 5) {
+        //                 event.preventDefault();
+
+        //                 $('#query-input-error-message').text(`Query sting must be at least 5 character long. "${queryText}" is only ${queryText.length} characters long`);
+
+        //             } else {
+        //                 // go to google!
+        //             }
+        //         }
+        //     });
+
+
+        // }
