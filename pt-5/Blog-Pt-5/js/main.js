6c414c084a06c007870dea7e94d1bf85c584ad99
emaquino44 <prexy44@yahoo.com>
2017-04-11 10:14:43 -0700

    homework - pt5

---
+++ pt-5/Blog-Pt-5/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready;
+
+
+$(function() $("#submitBtn").on("click", function(event));
+        var formFields = ($(".blank"));
+        var formError = ($(".errorMsg")); console.log(formFields);
+
+        event.preventDefault();
+
+        for (var i = 0; i < formFields.length; i++) {
+            if (!formFields[i].value) {
+                $(formFields[i].addClass("error"); $(formError[i].show();
+                    }
+
+                });
+            $(".blank").on("keydown", function() {
+                        var (forFields) = ($ ".blank");
+
+                        var formError = ($(“.error - message”));
+                        for (var i = 0; i < formFields.length; i++) {
+                            if (!formFields[i].value) {
+                                $(formFields[i].addClass("error"); $(formError[i].show();
+                                    }
+                                }
+                            });
